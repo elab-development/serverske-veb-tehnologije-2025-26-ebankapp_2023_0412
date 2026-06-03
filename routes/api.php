6a7b19ef275f43bc0aa1649b3a53cf2a9092cc36
Ivana Jovanovic <ivanajjovanovicc@gmail.com>
2026-06-03 18:32:31 +0200

    dodate rute u api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/status', function () {
+    return response()->json(['status' => 'API is running']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('accounts', AccountController::class);
+    Route::apiResource('transactions', TransactionController::class);
+    Route::apiResource('users', UserController::class);
+
+    Route::post('transfer', [TransactionController::class, 'transfer']);
+    Route::get('transactions/search', [TransactionController::class, 'search']);
+    Route::get('accounts/{id}/transactions', [TransactionController::class, 'byAccount']);
+
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+});
